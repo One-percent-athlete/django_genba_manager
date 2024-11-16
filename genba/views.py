7130df8e502816bd291ab0a223d23ac33e82966c
One-percent-athlete <one.percent.athlete@gmail.com>
2024-11-16 19:54:03 +0900

    Modify login and register page

--- genba/views.py
+++ genba/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 def home(request):
-    return render(request, "home.html")
+    if request.user.is_authenticated:
+        return render(request, "home.html")
+    else:
+        return redirect("login")
+
+def register(request):
+    return render(request, "register.html")
 
 def login(request):
     return render(request, "login.html")
