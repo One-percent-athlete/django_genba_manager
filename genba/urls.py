25fe6d3122e805b31e34963eeeb473be24d2c137
One-percent-athlete <one.percent.athlete@gmail.com>
2024-11-16 20:44:36 +0900

    Modify homepage

--- genba/urls.py
+++ genba/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from genba import views
 
 urlpatterns = [
-    path('<int:year>/<str:month>', views.home, name="home"),
-    path('login_user', views.login_user, name="login"),
-    path('logout_user', views.logout_user, name="logout"),
-    path('register_user', views.register_user, name="register"),
+    path('<int:year>/<int:month>/<int:day>', views.home, name="home"),
+    path('login_user/', views.login_user, name="login"),
+    path('logout_user/', views.logout_user, name="logout"),
+    path('register_user/', views.register_user, name="register"),
 ]
--- genba/views.py
+++ genba/views.py
@@ -2,15 +2,16 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .forms import RegisterForm
+import datetime
+x = datetime.datetime.now()
 
-def home(request, year, month):
+def home(request, year, month, day):
     if request.user.is_authenticated:
-
         context = {
-            "year": year,
-            "month": month
+            "year": x.year,
+            "month": x.month,
+            "date":x.day
         }
-
         return render(request, "home.html", context=context)
     else:
         return redirect("login")
@@ -22,7 +23,7 @@ def login_user(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return redirect("home")
+            return redirect("home", year=x.year, month=x.month, day=x.day)
         else:
             messages.success(request, ("Username Or Password Was Not Correct, Please Try Again."))
             return redirect("login")
