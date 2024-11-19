9f37e68c7b19808dabf53c5fb8b89decaade9951
One-percent-athlete <one.percent.athlete@gmail.com>
2024-11-19 14:28:42 +0900

    Create add user page, url and view

--- genba/urls.py
+++ genba/urls.py
@@ -13,4 +13,5 @@ urlpatterns = [
     path('login_user/', views.login_user, name="login_user"),
     path('logout_user/', views.logout_user, name="logout_user"),
     path('register_user/', views.register_user, name="register_user"),
+    path('add_user/', views.add_user, name="add_user"),
 ]
--- genba/views.py
+++ genba/views.py
@@ -93,5 +93,21 @@ def register_user(request):
         form = RegisterForm()
 
      return render(request, "authenticate/register.html", {"form": form})
+
+def add_user(request):
+     if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password1"]
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, ("Registration Successful!!"))
+            return redirect("login_user")
+     else:
+        form = RegisterForm()
+
+     return render(request, "authenticate/add_user.html", {"form": form})
     
         
