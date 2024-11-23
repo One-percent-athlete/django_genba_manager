78ef092b308e5827240c411261f37fb8fc71a334
One-percent-athlete <one.percent.athlete@gmail.com>
2024-11-23 21:19:44 +0900

    Create custom user model

--- genba/models.py
+++ genba/models.py
@@ -1,4 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+
+class CustomUser(AbstractBaseUser):
+    username = models.CharField(max_length=20, unique=True)
+    phone_number = models.CharField(max_length=20)
+    note = models.CharField(max_length=500)
+    is_staff = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.username} - {self.phone_number}"
+    
+    class Meta:
+        verbose_name_plural = "users"
 
 # class Genba(models.Model):
 #     name = models.CharField("Genba", max_length=255)
@@ -13,3 +29,5 @@ from django.db import models
 
 #     def __str__(self):
 #         return f"{self.name} - {self.client}"
+
+
