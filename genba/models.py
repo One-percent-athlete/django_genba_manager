68e22efb4180174f1a66cc98518d44b2fd7452a1
One-percent-athlete <one.percent.athlete@gmail.com>
2024-11-24 19:14:35 +0900

    Create forms

--- genba/models.py
+++ genba/models.py
@@ -3,68 +3,6 @@ from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 
-# class CustomUserManager(BaseUserManager):
-
-#     def create_user(self, username, fullname, phone_number, password, note):
-#         user = self.model(
-#             username=username,
-#             fullname=fullname,
-#             phone_number=phone_number,
-#             password=password,
-#             note=note
-#         )
-#         user.set_password(password)
-#         user.save()
-#         return user
-    
-#     def create_syain_user(self, username, fullname, phone_number, password, note):
-#         user = self.create_user(
-#             username=username,
-#             fullname=fullname,
-#             phone_number=phone_number,
-#             note=note
-#         )
-#         user.is_staff = True
-#         user.set_password(password)
-#         user.save()
-#         return user
-    
-#     def create_superuser(self, username, fullname, phone_number, password, note):
-#         user = self.create_user(
-#             username=username,
-#             fullname=fullname,
-#             phone_number=phone_number,
-#             note=note
-#         )
-#         user.is_staff = True
-#         user.is_superuser = True
-#         user.set_password(password)
-#         user.save()
-#         return user
-
-
-# class CustomUser(AbstractBaseUser):
-#     username = models.CharField(max_length=50, unique=True)
-#     fullname = models.CharField(max_length=20, unique=True)
-#     phone_number = models.CharField(max_length=20)
-#     note = models.CharField(max_length=500)
-#     is_staff = models.BooleanField(default=False)
-#     is_superuser = models.BooleanField(default=False)
-#     is_active = models.BooleanField(default=True)
-#     created_at = models.DateTimeField(auto_now_add=True)
-#     updated_at = models.DateTimeField(auto_now=True)
-
-#     objects = CustomUserManager()
-
-#     USERNAME_FIELD = "username"
-#     REQUIRED_FIELDS = ["fullname", "phone_number", "note"]
-
-#     def __str__(self):
-#         return f"{self.username} - {self.fullname} - {self.phone_number}"
-    
-#     class Meta:
-#         verbose_name_plural = "users"
-
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
