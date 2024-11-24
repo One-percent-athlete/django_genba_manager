f0571bce90b071329efe5c10793ae64a20981e7f
One-percent-athlete <one.percent.athlete@gmail.com>
2024-11-24 19:08:51 +0900

    Modify profile model

--- genba/admin.py
+++ genba/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-# from .models import Genba
+from .models import Profile
 
 # Register your models here.
-# admin.site.register(Genba)
+admin.site.register(Profile)
--- genba/models.py
+++ genba/models.py
@@ -68,18 +68,18 @@ from django.db.models.signals import post_save
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    date_modified = models.DateTimeField(User, auto_now=True)
-    phone = models.CharField(max_length=30, blank=True)
-    address1 = models.CharField(max_length=200, blank=True)
-    address2 = models.CharField(max_length=200, blank=True)
-    city = models.CharField(max_length=200, blank=True)
-    state = models.CharField(max_length=200, blank=True)
-    zipcode = models.CharField(max_length=200, blank=True)
-    country = models.CharField(max_length=200, blank=True)
-    old_cart = models.CharField(max_length=200, blank=True, null=True)
+    fullname = models.CharField(max_length=20, unique=True, blank=True)
+    phone_number = models.CharField(max_length=20, blank=True)
+    note = models.CharField(max_length=500, blank=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.user.username
+        return f"{self.user} - {self.fullname} - {self.phone_number}"
+    
 
 def create_profile(sender, instance, created, **kwargs):
     if created:
