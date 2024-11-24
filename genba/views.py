11e56bf93bcf8ffcbe7b9f93e81bc9eba5176e52
One-percent-athlete <one.percent.athlete@gmail.com>
2024-11-24 19:19:11 +0900

    Modify views. py file

--- genba/views.py
+++ genba/views.py
@@ -1,13 +1,67 @@
+from cProfile import Profile
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from django.utils.safestring import mark_safe
 import calendar
 import datetime
 x = datetime.datetime.now()
 
-from .forms import RegisterForm
+from .forms import RegisterForm, SignUpForm, UpdateUserForm, UserProfileForm
+
+def update_user(request, user_id):
+    if request.user.is_authenticated:
+        current_user = User.objects.get(id=user_id)
+        user_form = UpdateUserForm(request.POST or None, instance=current_user)
+
+        if user_form.is_valid():
+            user_form.save()
+            login(request, current_user)
+            messages.success(request, "Your Profile Has Been Updated Successfully.")
+            return redirect("update_user", user_id)
+        return render(request, "update_user.html", {"user_form": user_form})
+    else:
+        messages.success(request, "You Must Login First!")
+        return redirect("home")
+    
+def update_profile(request, user_id):
+    if request.user.is_authenticated:
+
+        current_user = Profile.objects.get(user__id=user_id)
+        form = UserProfileForm(request.POST or None, instance=current_user)
+        if form.is_valid():
+
+            form.save()
+            messages.success(request, "Your Profile Has Been Updated Successfully.")
+            return redirect("update_user", user_id)
+        return render(request, "update_profile.html", {"form": form})
+    else:
+        messages.success(request, "You Must Login First!")
+        return redirect("home")
+
+
+def add_user(request):
+    form = SignUpForm()
+
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password1"]
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, ("Welcome To Our Online Shop. Please Fill Out Your Profile."))
+            return redirect("update_profile", user.pk)
+        else:
+            messages.success(request, ("Whoops, There Was A Problem Registering, Please Try Agian.."))
+            return redirect("login_user")
+    else:
+        return render(request, "add_user.html", {
+            "form": form
+        })
 
 @login_required(login_url='/login_user/')
 def home(request):
@@ -33,7 +87,7 @@ def logout_user(request):
     messages.success(request, ("You Are Logged Out"))
     return redirect("login_user")
 
-def add_user(request):
+# def add_user(request):
     if request.user.is_authenticated:
         if request.method == "POST":
             form = RegisterForm(request.POST)
