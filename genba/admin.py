a54f6ab25e56b6674b3359aa3eea03c084bfe4b0
One-percent-athlete <one.percent.athlete@gmail.com>
2024-11-26 22:15:45 +0900

    Update models and forms

--- genba/admin.py
+++ genba/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
 from .models import Profile
 
 # Register your models here.
 admin.site.register(Profile)
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    fields = ['username']
+    inlines = [ProfileInline]
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
--- genba/models.py
+++ genba/models.py
@@ -6,10 +6,8 @@ from django.db.models.signals import post_save
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    first_name = models.CharField(max_length=20, blank=True)
-    last_name = models.CharField(max_length=20, blank=True)
+    fullname = models.CharField(max_length=20, blank=True)
     phone = models.CharField(max_length=20, blank=True)
-    email = models.EmailField(max_length=20, blank=True)
     note = models.CharField(max_length=500, blank=True)
     is_temp = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=True)
@@ -19,7 +17,7 @@ class Profile(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f"{self.user} - {self.first_name} - {self.last_name}"
+        return f"{self.user} - {self.fullname}"
     
 
 def create_profile(sender, instance, created, **kwargs):
