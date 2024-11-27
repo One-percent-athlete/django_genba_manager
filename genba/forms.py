ebda8143b82224ad89312ef0f184f9ec14d53e61
One-percent-athlete <one.percent.athlete@gmail.com>
2024-11-27 09:28:46 +0900

    Update forms and models

--- genba/forms.py
+++ genba/forms.py
@@ -4,10 +4,6 @@ from django import forms
 from .models import Profile
 
 class SignUpForm(UserCreationForm):
-	# Can be left blank here because the admin section does not require them
-	# email = forms.EmailField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Email Address'}))
-	# first_name = forms.CharField(label="", max_length=100, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'First Name'}))
-	# last_name = forms.CharField(label="", max_length=100, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Last Name'}))
 
 	class Meta:
 		model = User
@@ -17,51 +13,32 @@ class SignUpForm(UserCreationForm):
 		super(SignUpForm, self).__init__(*args, **kwargs)
 
 		self.fields['username'].widget.attrs['class'] = 'form-control'
-		self.fields['username'].widget.attrs['placeholder'] = 'Username'
+		self.fields['username'].widget.attrs['placeholder'] = 'ユーザー名'
 		self.fields['username'].label = ''
 		self.fields['username'].help_text = '<span class="form-text text-muted"><small>Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.</small></span>'
 
 		self.fields['password1'].widget.attrs['class'] = 'form-control'
-		self.fields['password1'].widget.attrs['placeholder'] = 'Password'
+		self.fields['password1'].widget.attrs['placeholder'] = 'パスワード'
 		self.fields['password1'].label = ''
 		self.fields['password1'].help_text = '<ul class="form-text text-muted small"><li>Your password can\'t be too similar to your other personal information.</li><li>Your password must contain at least 8 characters.</li><li>Your password can\'t be a commonly used password.</li><li>Your password can\'t be entirely numeric.</li></ul>'
 
 		self.fields['password2'].widget.attrs['class'] = 'form-control'
-		self.fields['password2'].widget.attrs['placeholder'] = 'Confirm Password'
+		self.fields['password2'].widget.attrs['placeholder'] = '確認パスワード'
 		self.fields['password2'].label = ''
 		self.fields['password2'].help_text = '<span class="form-text text-muted"><small>Enter the same password as before for verification.</small></span>'
-
-# class UpdateUserForm(UserChangeForm):
-# 		password = None
-# 		first_name = forms.CharField(label="", max_length=100, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'First name'}), required=False)
-# 		last_name = forms.CharField(label="", max_length=100, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Last name'}), required=False)
-# 		phone = forms.CharField(label="", max_length=100, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Phone Number'}))		
-
-# 		class Meta:
-# 			model = User
-# 			fields = ('username', 'first_name', 'last_name', 'phone')
-
-# 		def __init__(self, *args, **kwargs):
-# 			super(UpdateUserForm, self).__init__(*args, **kwargs)
-
-# 			self.fields['username'].widget.attrs['class'] = 'form-control'
-# 			self.fields['username'].widget.attrs['placeholder'] = 'Username'
-# 			self.fields['username'].label = ''
-# 			self.fields['username'].help_text = '<span class="form-text text-muted"><small>Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.</small></span>'
-
-
     
 class UserProfileForm(forms.ModelForm):
 	CHOICE = [
        	('元請', '元請'),
         ('正社員', '正社員'),
         ('管理', '管理'),]
-	fullname = forms.CharField(label="", max_length=100, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Fullname'}))
-	phone = forms.CharField(label="", max_length=100, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Phone Number'}))
+	fullname = forms.CharField(label="", max_length=100, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'お名前フルねーム、スペースなし'}))
+	phone = forms.CharField(label="", max_length=100, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'携帯電話番号'}))
 	note = forms.CharField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Note'}))
 	contract_type = forms.ChoiceField(label="雇用形態", choices=CHOICE, widget=forms.RadioSelect(attrs={'class': 'form-check-input'}))
+	is_active = forms.BooleanField(label="is_active", required=False)
 
 	class Meta:
 		model = Profile
-		fields = ('fullname', 'phone', 'note', 'contract_type')
+		fields = ('fullname', 'phone', 'note', 'contract_type', 'is_active')
 		
--- genba/models.py
+++ genba/models.py
@@ -30,18 +30,18 @@ def create_profile(sender, instance, created, **kwargs):
 
 post_save.connect(create_profile, sender=User)
 
-# class Genba(models.Model):
-#     name = models.CharField("Genba", max_length=255)
-#     client = models.CharField("Client", max_length=255)
-#     address = models.CharField("Address", max_length=255)
-#     job_description = models.CharField("Job description", max_length=255)
-#     remarks = models.CharField("Remarks", max_length=255)
-#     start_date = models.DateTimeField("Start date")
-#     end_date = models.DateTimeField("End date")
-#     person_in_charge = models.ForeignKey( on_delete=)
-#     date_created = models.DateTimeField(auto_now=True)
+class Genba(models.Model):
+    person_in_charge = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True)
+    name = models.CharField("Genba", max_length=255)
+    client = models.CharField("Client", max_length=255)
+    address = models.CharField("Address", max_length=255)
+    job_description = models.CharField("Job description", max_length=255)
+    remarks = models.CharField("Remarks", max_length=255)
+    start_date = models.DateTimeField("Start date")
+    end_date = models.DateTimeField("End date")
+    date_created = models.DateTimeField(auto_now=True)
 
-#     def __str__(self):
-#         return f"{self.name} - {self.client}"
+    def __str__(self):
+        return f"{self.name} - {self.client}"
 
 
