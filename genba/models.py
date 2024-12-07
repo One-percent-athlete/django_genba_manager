dd1545e7a5d3ad5b5bdc6e33e4d51844906a52f0
One-percent-athlete <one.percent.athlete@gmail.com>
2024-12-07 21:32:29 +0900

    Update model

--- genba/models.py
+++ genba/models.py
@@ -11,14 +11,14 @@ class Profile(models.Model):
         ('正社員', '正社員'),
         ('管理', '管理'),
     )
-    contract_type = models.CharField(max_length=50, choices=CONTRACT_TYPES, default='正社員')
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    fullname = models.CharField(max_length=20, blank=True)
-    phone = models.CharField(max_length=20, blank=True)
-    note = models.CharField(max_length=500, blank=True)
-    is_active = models.BooleanField(default=True)
-    date_created = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    contract_type = models.CharField("雇用形態", max_length=50, choices=CONTRACT_TYPES, default='正社員')
+    fullname = models.CharField("お名前", max_length=20, blank=True)
+    phone = models.CharField("携帯電話", max_length=20, blank=True)
+    note = models.CharField("備考", max_length=500, blank=True)
+    is_active = models.BooleanField("現役中", default=True)
+    date_created = models.DateTimeField("作成日", auto_now_add=True)
+    updated_at = models.DateTimeField("更新日", auto_now=True)
 
     def __str__(self):
         return f"{self.fullname}"
